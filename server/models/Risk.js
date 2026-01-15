e74fe37f33a93f82764e931dc4f290f81dd9d624
LuyandaDladla <luyandadl080@gmail.com>
2026-01-15 23:10:53 +0200

    Add missing token util + Risk model

---
+++ server/models/Risk.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+
+const RiskSchema = new mongoose.Schema(
+  {
+    user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: false },
+    province: { type: String, default: "" },
+    score: { type: Number, default: 0 },
+    answers: { type: Object, default: {} },
+    flags: { type: [String], default: [] },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("Risk", RiskSchema);
