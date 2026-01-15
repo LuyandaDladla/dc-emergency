ec0332eb8e344b389d8eadf9b4961dbec2658f54
LuyandaDladla <luyandadl080@gmail.com>
2026-01-15 23:17:43 +0200

    Fix Render crashes: Risk model + default connectDB export

--- server/config/db.js
+++ server/config/db.js
@@ -1,7 +1,13 @@
-﻿import mongoose from "mongoose";
+import mongoose from "mongoose";
 
-export async function connectDB(uri) {
+export default async function connectDB() {
   try {
+    const uri = process.env.MONGO_URI;
+    if (!uri) {
+      console.error("MONGO_URI is missing. Set it in Render environment variables.");
+      process.exit(1);
+    }
+
     await mongoose.connect(uri);
     console.log("MongoDB connected");
   } catch (err) {
