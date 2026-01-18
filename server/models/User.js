7c9b642658cd153b5b25bb4c81a505b23d404701
LuyandaDladla <luyandadl080@gmail.com>
2026-01-18 13:46:37 +0200

    fix: add User.matchPassword + hash passwords

--- server/models/User.js
+++ server/models/User.js
@@ -1,5 +1,6 @@
-import mongoose from "mongoose";
+﻿import mongoose from "mongoose";
 
+import bcrypt from "bcryptjs";
 const userSchema = new mongoose.Schema(
   {
     name: { type: String, default: "" },
@@ -15,4 +16,19 @@ const userSchema = new mongoose.Schema(
   { timestamps: true }
 );
 
+
+userSchema.pre("save", async function(next) {
+  try {
+    if (!this.isModified("password")) return next();
+
+userSchema.methods.matchPassword = async function(enteredPassword) {
+  return bcrypt.compare(enteredPassword, this.password);
+};
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    return next();
+  } catch (err) {
+    return next(err);
+  }
+});
 export default mongoose.model("User", userSchema);
