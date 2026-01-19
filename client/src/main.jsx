a5b47ea21394ec0d2759e5ac45a375a217ef7563
LuyandaDladla <luyandadl080@gmail.com>
2026-01-19 23:41:51 +0200

    pwa: add manifest + sw + icons

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,16 +1,9 @@
-﻿import React from "react";
-import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
-import "./index.css";
-import App from "./App";
-import { AuthProvider } from "./context/AuthContext";
+import { registerSW } from "virtual:pwa-register";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-    <React.StrictMode>
-        <BrowserRouter>
-            <AuthProvider>
-                <App />
-            </AuthProvider>
-        </BrowserRouter>
-    </React.StrictMode>
-);
+registerSW({ immediate: true });
+
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker.register("/sw.js").catch(() => {});
+  });
+}
